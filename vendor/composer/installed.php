3c87fb1ce4b6a9130e97a29000c97f6394a76ad9
unknown <=>
2024-09-01 16:54:55 +0100

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'oa/mvc_framework',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b1bbddef841fdbe9f708c559fb7bf67ef5c80b66',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oa/mvc_framework' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b1bbddef841fdbe9f708c559fb7bf67ef5c80b66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
